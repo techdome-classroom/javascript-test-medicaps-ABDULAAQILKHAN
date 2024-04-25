2517aa7a246d0e6d26c2d62711b4f87fc4eb11eb
Aaqil Khan <en21cs3l1003@medicaps.ac.in>
2024-04-25 13:24:31 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -7,11 +7,13 @@ function longestSubstring(text) {
         let start = 0;
     
         for (let i = 0; i < text.length; i++) {
+
             if (text[i] in allIndexObj && allIndexObj[text[i]] >= start) {
                 start = allIndexObj[text[i]] + 1;
             }
             allIndexObj[text[i]] = i;
             maxLength = Math.max(maxLength, i - start + 1);
+            
         }
     
         return maxLength;
