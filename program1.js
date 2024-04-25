d6a315124bd6295b3f81056b7ab8a32e70908fc2
Aaqil Khan <en21cs3l1003@medicaps.ac.in>
2024-04-25 13:24:22 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,15 +2,15 @@ function longestSubstring(text) {
     // Implementation of longestSubstring function
         if (text.length === 0) return 0;
     
-        let charIndexMap = {};
+        let allIndexObj = {};
         let maxLength = 0;
         let start = 0;
     
         for (let i = 0; i < text.length; i++) {
-            if (text[i] in charIndexMap && charIndexMap[text[i]] >= start) {
-                start = charIndexMap[text[i]] + 1;
+            if (text[i] in allIndexObj && allIndexObj[text[i]] >= start) {
+                start = allIndexObj[text[i]] + 1;
             }
-            charIndexMap[text[i]] = i;
+            allIndexObj[text[i]] = i;
             maxLength = Math.max(maxLength, i - start + 1);
         }
     
