cfc586e2bb0909e1b7b515eb1bde67170ff01624
Aaqil Khan <en21cs3l1003@medicaps.ac.in>
2024-04-25 13:59:26 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -26,9 +26,9 @@ function smallestMissingPositiveInteger(nums) {
 }
 
 // Example usage:
-let arr = [3, 4, -1, 1];
-let arr2 = [-1, -3, 4, 2];
-let arr3 = [1, 2, 0];
+//let arr = [3, 4, -1, 1];
+//let arr2 = [-1, -3, 4, 2];
+//let arr3 = [1, 2, 0];
 
 //console.log(smallestMissingPositiveInteger(arr));
 //console.log(smallestMissingPositiveInteger(arr2));
